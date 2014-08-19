c02cd47bc8c67e312f98bc76baa4878eea3125f7
sri-m <srinivaspkl@gmail.com>
2014-08-19 18:58:41 +0530

    Changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,8 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
+config.serve_static_assets = true
+config.assets.compile = true
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -20,14 +21,14 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
